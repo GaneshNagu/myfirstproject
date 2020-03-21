2bad124a978a74620be2bb5875d3b3eee0c58636
kingkalinga <rajunaga57@gmail.com>
2020-03-21 20:28:01 +0530

    practise segment 38

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const person=(props)=>{
+
+    return(
+        <div>
+        <p>Iam {props.name} and iam {props.age} years old</p>
+         </div>
+    )
+
+}
+export default person;
